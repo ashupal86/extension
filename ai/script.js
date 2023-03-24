2fd12128f41bdadabbaff1dbd84c7befb17354cb
Ashu Bro <mr.ashu1810@gmail.com>
2023-03-24 18:09:00 +0530

    chat extension version 1.0.0 completed

---
+++ ai/script.js
@@ -0,0 +1,34 @@
+const apiKey = "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";
+const query = document.getElementById("query");
+const results="";
+function btnclick(apiKey,query,results){
+const params = {
+  model: "davinci",
+  documents: [],
+  max_rerank: 10,
+  return_metadata: true,
+};
+
+const requestOptions = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${apiKey}`
+  },
+  body: JSON.stringify({
+    query: query,
+    ...params
+  })
+};
+
+fetch('https://api.openai.com/v1/search', requestOptions)
+  .then(response => response.json())
+  .then(data => {
+    results = data.data;
+    console.log(results);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
+document.getElementById("answer").innerHTML=results;
